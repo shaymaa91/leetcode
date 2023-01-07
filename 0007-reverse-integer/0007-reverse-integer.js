a6ab036237bebbbb55fcfdfbd09e521d1135b8e2
shaymaa khalaf <61957173+shaymaa91@users.noreply.github.com>
2023-01-07 21:12:40 +0300

    Time: 73 ms (90.66%), Space: 44.2 MB (32.70%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -9,20 +9,16 @@ var reverse = function(x) {
     if(s[0]!=="-"){
         s.unshift("+");
     }
-    console.log(s);
     let temp;
     for(let i=1,j=s.length-1;i<s.length/2;i++,j--){
         temp=s[i];
         s[i]=s[j];
         s[j]=temp
     }
-    console.log(s)
     let result = Number(s.reduce((i,current)=>{
         return i+current
     }));
-    console.log(result)
-    if(result> Math.pow(2, 31) || result< Math.pow(-2, 31)){
-        
+    if(result> Math.pow(2, 31) || result< Math.pow(-2, 31)){        
         return 0}
     
     return(result)
