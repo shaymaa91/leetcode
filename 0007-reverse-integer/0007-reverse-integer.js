b25bae2f6e075ac969e2b99605f68cf8cf9f767a
shaymaa khalaf <61957173+shaymaa91@users.noreply.github.com>
2023-01-08 00:23:39 +0300

    Time: 131 ms (18.94%), Space: 48.6 MB (5.20%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,23 +3,37 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let s = ""+x;
-    s=s.split("");
+//     let s = ""+x;
+//     s=s.split("");
     
-    if(s[0]!=="-"){
-        s.unshift("+");
+//     if(s[0]!=="-"){
+//         s.unshift("+");
+//     }
+//     let temp;
+//     for(let i=1,j=s.length-1;i<s.length/2;i++,j--){
+//         temp=s[i];
+//         s[i]=s[j];
+//         s[j]=temp
+//     }
+//     let result = Number(s.reduce((i,current)=>{
+//         return i+current
+//     }));
+//     if(result> Math.pow(2, 31) || result< Math.pow(-2, 31)){        
+//         return 0}
+    
+//     return(result)
+    
+    //splution 2
+    let reversedNum=0;
+    let reminder;
+    
+    while(x!=0){
+        reminder = x%10;
+        x=x/10 - reminder/10;
+        reversedNum= reversedNum*10 +reminder;
+        console.log(x,reminder)
     }
-    let temp;
-    for(let i=1,j=s.length-1;i<s.length/2;i++,j--){
-        temp=s[i];
-        s[i]=s[j];
-        s[j]=temp
-    }
-    let result = Number(s.reduce((i,current)=>{
-        return i+current
-    }));
-    if(result> Math.pow(2, 31) || result< Math.pow(-2, 31)){        
+    if(reversedNum> Math.pow(2, 31) || reversedNum< Math.pow(-2, 31)){        
         return 0}
-    
-    return(result)
+    return reversedNum
 };
