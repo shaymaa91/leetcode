21a975a84a304d4b43f89278f85b1eb9b09465f2
shaymaa khalaf <61957173+shaymaa91@users.noreply.github.com>
2023-01-08 14:14:12 +0300

    Time: 65 ms (96.44%), Space: 43 MB (30.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = [];
-    for(let i = 0 ; i<nums.length-1; i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){result.push(i,j); break;}
+   
+    let m = new Map();
+    for(let i=0;i<nums.length;i++){
+       
+        if(m.get(nums[i])!= undefined){
+            return [m.get(nums[i]),i];
+        }else{
+            m.set(target-nums[i],i);
         }
     }
-    return result
+    
+
+  
 };
